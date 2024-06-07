e78811bc9106a3f5a2284ba2ff2b20b2a80d3d35
Michał Pasynek <terabajt@terabajtlw.pl>
2024-06-07 14:46:32 +0200

    [featured] Added property details page.

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
-import properties from '@/properties.json';
 import PropertyCard from '@/components/PropertyCard';
+import { fetchProperties } from '@/utils/requests';
 
-const PropertyPage = () => {
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+
+  // Sort properties by date
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
   return (
-    <section className="px-4 py-6">
-    <div className="container-xl lg:container m-auto px-4 py-6">
-       { properties.length === 0 ? (
-        <p>No properties found</p>
-       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        { properties.map((property) => (
-         <PropertyCard key={property._id} property = {
-          property
-         }>
-          s
-         </PropertyCard>
-        ))}
-        </div>
-       ) }
-    
-        </div>
-        </section>
-  )
-}
-
-export default PropertyPage
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+export default PropertiesPage;
--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -76,7 +76,8 @@ const PropertyCard = ( { property }) => {
           <span className="text-orange-700"> { property.location.city } { property.location.state } {' '} </span>
         </div>
         <Link
-          href={ `/properties/${property._id}` }
+          href={ `/properties/${property._id}`
+         }
           className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
           Details
