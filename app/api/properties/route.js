ea3593ba19cebfcaccc6aea790d6c8c15128c402
Michał Pasynek <terabajt@terabajtlw.pl>
2024-06-14 12:47:05 +0200

    [featured] Created form data from property data.

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -15,3 +15,44 @@ export const GET = async request => {
 		return new Response('Something went wrong: ', { status: 500 });
 	}
 };
+
+export const POST = async request => {
+	try {
+		const formData = await request.formData();
+
+		// Access all values from amenities and images
+		const amenities = formData.getAll('amenities');
+		const images = formData.getAll('image').filter(img => image.name !== '');
+
+		// Create propertyData object for database
+		const propertyData = {
+			type: formData.get('type'),
+			name: formData.get('name'),
+			description: formData.get('description'),
+			location: {
+				street: formData.get('location.street'),
+				city: formData.get('location.city'),
+				state: formData.get('location.state'),
+				zipcode: formData.get('location.zipcode'),
+			},
+			beds: formData.get('beds'),
+			baths: formData.get('baths'),
+			square_feet: formData.get('square_feet'),
+			amenities,
+			rates: {
+				weekly: formData.get('weekly'),
+				monthly: formData.get('monthly'),
+				nightly: formData.get('nightly'),
+			},
+			seller_info: {
+				name: formData.get('name'),
+				email: formData.get('email'),
+				phone: formData.get('phone'),
+			},
+			images,
+		};
+		return new Response(JSON.stringify({ message: 'Success' }), { status: 200 });
+	} catch (error) {
+		return new Response('Failed to add property' + error, { status: 500 });
+	}
+};
--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -101,7 +101,7 @@ const PropertyAddForm = () => {
         }))
     };
   return mounted && (
-    <form>
+    <form action='/api/properties' method='POST' encType='multipart/form-data'>
     <h2 className="text-3xl text-center font-semibold mb-6">
       Add Property
     </h2>
@@ -494,7 +494,7 @@ const PropertyAddForm = () => {
       <input
         type="text"
         id="seller_name"
-        name="seller_info.name."
+        name="seller_info.name"
         className="border rounded w-full py-2 px-3"
         placeholder="Name"
         value={fields.seller_info.name}
