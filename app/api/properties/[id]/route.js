68fcced9ed7603b972ab16a52f8219714d8f762c
Michał Pasynek <terabajt@terabajtlw.pl>
2024-06-20 13:57:01 +0200

    [featured] Added map and geolocation with pin

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -5,23 +5,42 @@ import { getSessionUser } from '@/utils/getSessionUser';
 // GET /api/properties/:id
 export const GET = async (request, { params }) => {
 	try {
+		await connectDB();
+
+		const property = await Property.findById(params.id);
+
+		if (!property) return new Response('Property Not Found', { status: 404 });
+
+		return new Response(JSON.stringify(property), {
+			status: 200,
+		});
+	} catch (error) {
+		console.log(error);
+		return new Response('Something Went Wrong', { status: 500 });
+	}
+};
+
+// DELETE /api/properties/:id
+export const DELETE = async (request, { params }) => {
+	try {
 		const propertyId = params.id;
-		const sessionUser = getSessionUser();
+
+		const sessionUser = await getSessionUser();
+
 		// Check for session
-		if (!sessionUser || !(await sessionUser).userId) {
+		if (!sessionUser || !sessionUser.userId) {
 			return new Response('User ID is required', { status: 401 });
 		}
-		const { userId } = sessionUser();
+
+		const { userId } = sessionUser;
 
 		await connectDB();
 
 		const property = await Property.findById(propertyId);
-		if (!property) {
-			return new Response('Property not found', { status: 404 });
-		}
+
+		if (!property) return new Response('Property Not Found', { status: 404 });
 
 		// Verify ownership
-
 		if (property.owner.toString() !== userId) {
 			return new Response('Unauthorized', { status: 401 });
 		}
@@ -33,24 +52,77 @@ export const GET = async (request, { params }) => {
 		});
 	} catch (error) {
 		console.log(error);
-		return new Response('Something went wrong: ', { status: 500 });
+		return new Response('Something Went Wrong', { status: 500 });
 	}
 };
 
-// Delete /api/properties/:id
-export const DELETE = async (request, { params }) => {
+// PUT /api/properties/:id
+export const PUT = async (request, { params }) => {
 	try {
 		await connectDB();
 
-		const property = await Property.findById(params.id);
-		if (!property) {
-			return new Response('Property not found', { status: 404 });
+		const sessionUser = await getSessionUser();
+
+		if (!sessionUser || !sessionUser.userId) {
+			return new Response('User ID is required', { status: 401 });
 		}
-		return new Response(JSON.stringify(property), {
+
+		const { id } = params;
+		const { userId } = sessionUser;
+
+		const formData = await request.formData();
+
+		// Access all values from amenities
+		const amenities = formData.getAll('amenities');
+
+		// Get property to update
+		const existingProperty = await Property.findById(id);
+
+		if (!existingProperty) {
+			return new Response('Property does not exist', { status: 404 });
+		}
+
+		// Verify ownership
+		if (existingProperty.owner.toString() !== userId) {
+			return new Response('Unauthorized', { status: 401 });
+		}
+
+		// Create propertyData object for database
+		const propertyData = {
+			type: formData.get('type'),
+			name: formData.get('name'),
+			description: formData.get('description'),
+			location: {
+				street: formData.get('location.street'),
+				city: formData.get('location.city'),
+				state: formData.get('location.state'),
+				zipcode: formData.get('location.zipcode'),
+			},
+			beds: formData.get('beds'),
+			baths: formData.get('baths'),
+			square_feet: formData.get('square_feet'),
+			amenities,
+			rates: {
+				weekly: formData.get('rates.weekly'),
+				monthly: formData.get('rates.monthly'),
+				nightly: formData.get('rates.nightly.'),
+			},
+			seller_info: {
+				name: formData.get('seller_info.name'),
+				email: formData.get('seller_info.email'),
+				phone: formData.get('seller_info.phone'),
+			},
+			owner: userId,
+		};
+
+		// Update property in database
+		const updatedProperty = await Property.findByIdAndUpdate(id, propertyData);
+
+		return new Response(JSON.stringify(updatedProperty), {
 			status: 200,
 		});
 	} catch (error) {
 		console.log(error);
-		return new Response('Something went wrong: ', { status: 500 });
+		return new Response('Failed to add property', { status: 500 });
 	}
 };
