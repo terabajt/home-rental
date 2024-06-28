053dc6309c08f5e54c362c7cf02b366abec45123
Michał Pasynek <terabajt@terabajtlw.pl>
2024-06-28 22:13:37 +0200

    [feat] Added pagination to properties page.

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,27 +1,16 @@
-import React from "react";
-import PropertyCard from "@/components/PropertyCard";
-import { fetchProperties } from "@/utils/requests";
+import PropertySearchForm from '@/components/PropertySearchForm';
+import Properties from '@/components/Properties';
 
 const PropertiesPage = async () => {
-  const properties = await fetchProperties();
-
-  // Sort properties by date
-  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
-  return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>No properties found</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
-  );
+	return (
+		<>
+			<section className="bg-blue-700 py-4">
+				<div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+					<PropertySearchForm />
+				</div>
+			</section>
+			<Properties />
+		</>
+	);
 };
 export default PropertiesPage;
