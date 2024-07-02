cd51d17c872d49d4859d234ee72e363df801a99f
Michał Pasynek <terabajt@terabajtlw.pl>
2024-07-02 09:28:45 +0200

    [feat] Added featured property component.

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,18 @@
-import Hero from "@/components/Hero";
-import HomeProperties from "@/components/HomeProperties";
-import InfoBoxes from "@/components/InfoBoxes";
-import React from "react";
+import FeaturedProperties from '@/components/FeaturedProperties';
+import Hero from '@/components/Hero';
+import HomeProperties from '@/components/HomeProperties';
+import InfoBoxes from '@/components/InfoBoxes';
+import React from 'react';
 
 const HomePage = () => {
-  return (
-    <>
-      <Hero />
-      <InfoBoxes />
-      <HomeProperties />
-    </>
-  );
+	return (
+		<>
+			<Hero />
+			<InfoBoxes />
+			<FeaturedProperties />
+			<HomeProperties />
+		</>
+	);
 };
 
 export default HomePage;
