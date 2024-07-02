ab018111de6aa5f084cc6b141d3b19baab809bed
Michał Pasynek <terabajt@terabajtlw.pl>
2024-07-02 12:20:16 +0200

    [bug] "Refactor HomeProperties component to fetch and display recent properties"

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -3,9 +3,11 @@ import PropertyCard from '@/components/PropertyCard';
 import { fetchProperties } from '@/utils/requests';
 
 const HomeProperties = async () => {
+	let recentProperties = []; // empty array
 	const data = await fetchProperties();
-
-	const recentProperties = data.properties.sort(() => Math.random() - Math.random()).slice(0, 3);
+	if (data && Array.isArray(data.properties) && data.properties.length > 0) {
+		recentProperties = data.properties.sort(() => Math.random() - Math.random()).slice(0, 3);
+	}
 
 	return (
 		<>
@@ -13,7 +15,7 @@ const HomeProperties = async () => {
 				<div className="container-xl lg:container m-auto">
 					<h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">Recent Properties</h2>
 					<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-						{recentProperties === 0 ? (
+						{recentProperties.length === 0 ? (
 							<p>No Properties Found</p>
 						) : (
 							recentProperties.map(property => <PropertyCard key={property._id} property={property} />)
@@ -32,4 +34,5 @@ const HomeProperties = async () => {
 		</>
 	);
 };
+
 export default HomeProperties;
